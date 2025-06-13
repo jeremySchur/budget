fc755a03fe657095be4aba774a20ca4c652e5a3d
Jeremy Schur <jeremy.schur@telosalliance.com>
2025-06-12 20:04:07 -0600

    Added default expenses section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import Expenses from './components/Expenses';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="w-screen bg-primary p-4">
+    <div className="w-screen h-screen bg-primary p-5">
       <Header />
       <hr className="border-t border-box-color-light mt-4 w-full" />
+
+      <section className="flex my-20 h-3/4">
+        <Expenses />
+      </section>
     </div>
   );
 };
